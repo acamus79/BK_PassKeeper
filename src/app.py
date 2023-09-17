0c43656ae3fc210118c2d4eed509964d8ecd9b81
Adrian Edgardo Camus <adrianedgardocamus@gmail.com>
2023-09-17 18:39:06 -0300

    Rutas

--- src/app.py
+++ src/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
 from config import config
 # Routes
-from routes import Password
+from routes import PasswordRoutes
+from routes import AuthRoutes
 
 app = Flask(__name__)
 
@@ -11,8 +13,14 @@ def page_not_found(error):
 if __name__ == '__main__':
     app.config.from_object(config['development']) # This is the line that does the magic
     
+    jwt = JWTManager(app)  # Inicializa JWTManager con tu aplicación
+    
+    # Configura la clave secreta del JWTManager
+    app.config['JWT_SECRET_KEY'] = config['development'].JWT_SECRET_KEY
+    
     # Blueprints
-    app.register_blueprint(Password.main, url_prefix='/api/pass')
+    app.register_blueprint(PasswordRoutes.main, url_prefix='/api/v1/')
+    app.register_blueprint(AuthRoutes.auth_routes, url_prefix='/api/v1/auth')
     
     # Error handlers
     app.register_error_handler(404, page_not_found)
