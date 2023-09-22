131b6f2cccdebb8c33b021c7b97888d3b91a4493
Adrian Edgardo Camus <adrianedgardocamus@gmail.com>
2023-09-21 22:15:15 -0300

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,3 +1,3 @@
-bind = '0.0.0.0:5000'  # Especifica la dirección y el puerto en el que Gunicorn debe escuchar
+bind = '0.0.0.0:8000'  # Especifica la dirección y el puerto en el que Gunicorn debe escuchar
 workers = 4  # Número de trabajadores Gunicorn
 timeout = 60  # Tiempo de espera en segundos para las solicitudes
