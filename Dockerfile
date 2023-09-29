d1083349e8659dbf860d76aba8237db68d53b9f6
Adrian Edgardo Camus <adrianedgardocamus@gmail.com>
2023-09-29 20:01:35 -0300

    add

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,12 @@ FROM python:3.11
 WORKDIR /code/src
 
 # Copia el archivo de configuración de Gunicorn
-COPY gunicorn_config.py /code/gunicorn_config.py
+#COPY gunicorn_config.py /code/gunicorn_config.py
 
 COPY ./requirements.txt /code/requirements.txt
 RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
 COPY ./src /code/src
 
 # Ejecuta Gunicorn con la configuración proporcionada
-CMD ["gunicorn", "-c", "/code/gunicorn_config.py", "main:app"]
+#CMD ["gunicorn", "-c", "/code/gunicorn_config.py", "main:app"]
+CMD ["python", "app.py"]
--- src/database/db.py
+++ src/database/db.py
@@ -6,10 +6,10 @@ def get_connection():
     try:
         connection = psycopg2.connect(
             user=config('DB_USER', default='postgres'), 
-            password=config('DB_PASSWORD', default='root'),
+            password=config('DB_PASSWORD', default='postgres'),
             host=config('DB_HOST', default='localhost'),
             port=config('DB_PORT', default='5432'),
-            database=config('DB_NAME', default='passkeeper')
+            database=config('DB_NAME', default='postgres')
         )
         return connection
     except (Exception, DatabaseError) as error:
